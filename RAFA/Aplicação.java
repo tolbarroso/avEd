c77d040daab816d18ea5cd0a69b0424690446fbe
Rafriste <jrafaelprl@gmail.com>
2022-11-14 23:42:16 -0300

    Commitada de Rafinha

---
+++ RAFA/Aplicação.java
@@ -0,0 +1,73 @@
+public class Aplicação {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        LDECircular lista1 = new LDECircular();
+        LDECircular lista2 = new LDECircular();
+        LDECircular lista3 = new LDECircular();
+        LDECircular lista4 = new LDECircular();
+        int op;
+        int val;
+        String matricula;
+        String nome;
+        int faltas;
+        double media;
+        do {
+            exibeMenu();
+            op = in.nextInt();
+            in.nextLine();
+            switch (op) {
+                case 1: // inserir primeiro
+                    System.out.print("Informe o valor a ser inserido: ");
+                    val = in.nextInt();
+                    lista.inserirNoInicio(val);
+                    break;
+                case 2: // inserir último
+                    System.out.print("Informe o valor a ser inserido: ");
+                    val = in.nextInt();
+                    lista.inserirNoFinal(val);
+                    break;
+                case 3: // exibir todos
+                    lista.exibirTodos();
+                    break;
+                case 4: // exibir inverso
+                    lista.exibirInverso();
+                    break;
+                case 5: // remover valor especíico
+                    lista.exibirTodos();
+                    System.out.print("Informe o valor a ser removido: ");
+                    val = in.nextInt();
+                    lista.remover(val);
+                    break;
+                case 6: // remover primeiro
+                    System.out.print("Informe o valor a ser inserido: ");
+                    val = in.nextInt();
+                    lista.removerPrimeiro(val);
+                    break;
+                case 7: // remover último
+                    System.out.print("Informe o valor a ser inserido: ");
+                    val = in.nextInt();
+                    lista.removerUltimo(val);
+                    break;
+                case 0:
+                    System.out.println("Programa Encerrado!");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        } while (op != 0);
+    }
+
+    public static void exibeMenu() {
+        System.out.println("Opções");
+        System.out.println("1 - Inserir valor no início");
+        System.out.println("2 - Inserir valor no final");
+        System.out.println("3 - Exibir todos os valores");
+        System.out.println("4 - Exibir todos os valores inversamente");
+        System.out.println("5 - Remover valor específico da lista");
+        System.out.println("6 - Remover o primeiro valor");
+        System.out.println("7 - Remover o último valor");
+        System.out.println("0 - Encerrar programa");
+        System.out.print("Digite a opção desejada: ");
+    }
+}
+
---
+++ RAFA/LDECircular.java
@@ -0,0 +1,183 @@
+public class LDECircular<T extends Comparable<T>> {
+    private LDENode<T> prim;
+    private LDENode<T> ult;
+    private int qtd;
+
+    public boolean isEmpty() {
+        if (this.qtd == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void inserirNoInicio(T valor) {
+        LDENode<T> novo = new LDECNode(valor);
+        if (this.isEmpty() == true) {
+            this.prim = novo;
+            this.ult = novo;
+            this.qtd++;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else {
+            novo.setProx(this.prim);
+            this.prim.setAnt(novo);
+            this.prim = novo;
+            this.qtd++;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        }
+    }
+
+    public void inserirNoFinal(T valor) {
+        LDENode<T> novo = new LDENode(valor);
+        if (this.isEmpty() == true) {
+            this.prim = novo;
+            this.ult = novo;
+            this.qtd++;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else {
+            novo.setAnt(this.ult);
+            this.ult.setProx(novo);
+            this.ult = novo;
+            this.qtd++;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        }
+    }
+
+    public void exibirTodos() {
+        LDENode<T> aux;
+        if (this.isEmpty() == true) {
+            System.out.println("Lista Vazia!");
+        } else {
+            aux = this.prim;
+            do {
+                System.out.print(aux.getInfo() + " ");
+                aux = aux.getProx();
+            } while (aux != this.prim);
+        }
+        System.out.println(" ");
+    }
+
+    public void exibirInverso() {
+        LDENode<T> aux;
+        if (this.isEmpty() == true) {
+            System.out.println("Lista Vazia!");
+        } else {
+            aux = this.ult;
+            do {
+                System.out.print(aux.getInfo() + " ");
+                aux = aux.getAnt();
+            } while (aux != this.ult);
+        }
+        System.out.println(" ");
+    }
+
+    public LDENode<T> buscar(T valor) { // busca simples
+        LDENode<T> aux = this.prim;
+        if (this.isEmpty() == true) {
+            return null;
+        }
+        if (valor.compareTo(this.ult.getInfo()) == 0) {
+            return this.ult;
+        }
+        for (int i = 0; i < this.qtd; i++) {
+            if (valor.compareTo(aux.getInfo()) == 0) {
+                return aux;
+            }
+            aux = aux.getProx();
+        }
+        return null;
+    }
+
+    public void removerPrimeiro(T valor) { // remover o primeiro da lista
+        if (this.isEmpty() == true) {
+            System.out.println("Lista Vazia!");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.ult = null;
+            this.qtd = 0;
+        } else {
+            this.prim = this.prim.getProx();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        }
+    }
+
+    public void removerUltimo(T valor) { // remover o último valor da lista
+        if (this.isEmpty() == true) {
+            System.out.println("Lista Vazia!");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.ult = null;
+            this.qtd = 0;
+        } else {
+            this.ult = this.ult.getAnt();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        }
+    }
+
+    public void remover (T valor) { // Remove um valor específico
+        LDENode<T> retorno = this.buscar(valor);
+        LDENode<T> anterior, proximo;
+        if (retorno == null) {
+            System.out.println("valor não encontrado!");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.ult = null;
+            this.qtd = 0;
+        } else if (retorno == this.prim) { // remove o primeiro
+            this.prim = this.prim.getProx();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else if (retorno == this.ult) { // remove o ultimo
+            this.ult = this.ult.getAnt();
+            this.qtd--;
+            this.prim.setAnt(this.ult);
+            this.ult.setProx(this.prim);
+        } else { // remove no "meio"
+            anterior = retorno.getAnt();
+            proximo = retorno.getProx();
+            anterior.setProx(proximo);
+            proximo.setAnt(anterior);
+            this.qtd--;
+        }
+    }
+
+    public void remover2 (T valor) {
+        LDENode<T> aux, anterior, proximo;
+        if (this.isEmpty() == true) { // Caso 1: lista vazia!
+            System.out.println("Lista vazia");
+        } else if (this.qtd == 1) { // Caso 2: lista com apenas um nó
+            if (valor.compareTo(this.prim.getInfo()) == 0) {
+                this.prim = null;
+                this.ult = null;
+                this.qtd--;
+            } else {
+                System.out.println("Valor não encontrado");
+            }
+        } else { // Caso 3: caso geral (lista com mais de um nó)
+            aux = this.buscar(valor);
+            if (aux != null) {
+                anterior = aux.getAnt();
+                proximo = aux.getProx();
+                anterior.setProx(proximo);
+                proximo.setAnt(anterior);
+                this.qtd--;
+                if (aux == this.prim) {
+                    this.prim = proximo;
+                } else if (aux == this.ult) {
+                    this.ult = anterior;
+                }
+            } else {
+                System.out.println("Valor não encontrado");
+            }
+        }
+    }
+}
