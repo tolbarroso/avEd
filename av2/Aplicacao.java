f39c4cb6766dbdebecaca3c82491df05ef71e857
Caio Vilela <caio.2020200519@unicap.br>
2022-12-07 00:18:35 -0300

    COMMIT

--- av2/Aplicacao.java
+++ av2/Aplicacao.java
@@ -4,41 +4,34 @@ import java.util.Scanner;
 
 public class Aplicacao {
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
+        Scanner in = new Scanner(System.in);
         CadastroProdutos produtos = new CadastroProdutos();
         int op;
         do {
             exibirOpcoes();
             op = in.nextInt();
 
-            if (op == 1){
+            if (op == 1) {
                 produtos.cadastrarProduto();
-                break;
-            } else if (op == 2){
+            } else if (op == 2) {
                 produtos.exibirTodos();
-                break;
             } else if (op == 3) {
                 produtos.alterarPreco();
-                break;
-            } else if (op == 4){
+            } else if (op == 4) {
                 produtos.alterarQuantidade();
-                break;
             } else if (op == 5) {
                 produtos.exibirUm();
-                        break;
-            } else if (op == 6){
+            } else if (op == 6) {
                 produtos.removerProduto();
-                break;
             } else if (op == 0) {
                 System.out.println("bye");
             } else {
                 System.out.println("opção inválida");
-                break;
             }
         } while (op != 0);
     }
-    
-    public static void exibirOpcoes () {
+
+    public static void exibirOpcoes() {
         System.out.println(" ");
         System.out.println("1 - cadastro de produto");
         System.out.println("2 - exibir info de todos");
@@ -47,6 +40,6 @@ public class Aplicacao {
         System.out.println("5 - exibir info de um produto");
         System.out.println("6 - remoção de produto");
         System.out.println("0 - encerrar");
-        System.out.println("informe uma opção: ");   
+        System.out.println("informe uma opção: ");
     }
 }
